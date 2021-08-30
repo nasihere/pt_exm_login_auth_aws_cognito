d6b6f82f7b281c4142072fbca34c3c758562c725
NasirSayed <nasihere@gmail.com>
2021-08-30 14:33:51 -0400

    middlware added for checking token

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -12,6 +12,43 @@ class AuthMiddleware {
   constructor() {
     this.setUp()
   }
+  public verifyToken(req: Request, resp: Response, next): void {
+    console.log("Middleware", req.path)
+    const nonSecurePaths = ['/', '/auth/signin', '/auth/signup','/auth/verify'];
+    if (nonSecurePaths.includes(req.path)) return next();
+    
+    
+    const  token = req.header("authorization");
+    if (!token) {
+      console.log("No token found");
+      return resp.status(401).end();
+    }
+
+    let decodedJwt: any = jwt.decode(token, { complete: true });
+    if (decodedJwt === null) {
+      console.log("verify expired");
+      resp.status(401).end()
+      return
+    }
+    console.log(decodedJwt)
+    let kid = decodedJwt.header.kid;
+    let pem = pems[kid];
+    console.log(pem)
+    if (!pem) {
+      console.log("verify expired");
+      resp.status(401).end()
+      return
+    }
+    jwt.verify(token, pem, function (err: any, payload: any) {
+      if (err) {
+        console.log("verify expired", err);
+        resp.status(401).end()
+        return
+      } else {
+        next()
+      }
+    })
+  }
 
 
   private async setUp() {
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import App from './app'
 import * as bodyParser from 'body-parser'
 
 import AuthController from './controllers/auth.controller'
-
+import AuthMiddleware from './middleware/auth.middleware'
 
 const app = new App({
     port: 5000,
@@ -13,6 +13,7 @@ const app = new App({
     middleWares: [
       // parse application/json
       // parse application/x-www-form-urlencoded
+        new AuthMiddleware().verifyToken,
         bodyParser.json(),
         bodyParser.urlencoded({ extended: true }),
     ]
